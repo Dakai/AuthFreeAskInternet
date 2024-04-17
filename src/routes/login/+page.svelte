8a7c51aefa0ada419ea5dc1810a400c96cab2daf
dakai <oudakai@gmail.com>
2024-04-17 19:21:05 +0800

    Add browser check and title to login page; update Tailwind config

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { enhance } from '$app/forms';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
-  import { Toast } from 'flowbitesvelte';
 
-  export let form: any;
-  const 
+	export let form: any;
+	const message = writable('');
 
+	onMount(() => {
+		if (!browser) return;
+	});
+
+	$: if (form?.error) {
+	}
 </script>
+
+<svlete:head>
+	<title>Free Ask Internet Login</title>
+</svlete:head>

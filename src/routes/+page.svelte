8d1dffb9bc0f9bfe4da0d707761f4c889747b7eb
dakai <oudakai@gmail.com>
2024-04-18 00:36:10 +0800

    Add docker-compose.yml with authfreeaskinternet service

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,10 @@
 	<title>Free Ask Internet</title>
 </svelte:head>
 <div class="h-svh w-svw">
-	<iframe title="Free Ask Internet" width="100%" height="100%" src="http://localhost:3000" />
+	<iframe
+		title="Free Ask Internet"
+		width="100%"
+		height="100%"
+		src="http://host.docker.internal:3000"
+	/>
 </div>

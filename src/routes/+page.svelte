422bf22ffb3a5ea18009f8a1786bf03b1e21b8ef
dakai <oudakai@gmail.com>
2024-04-18 00:11:19 +0800

    Update page title and add iframe for Free Ask Internet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Free Ask Internet</title>
+</svelte:head>
+<div class="h-svh w-svw">
+	<iframe title="Free Ask Internet" width="100%" height="100%" src="http://localhost:3000" />
+</div>
